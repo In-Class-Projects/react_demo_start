c8c6eab211e839e5509f3a69c07efbf1834af880
PasinduMshan <madushan01722@gmail.com>
2024-11-27 15:35:58 +0530

    can't assign type name to content when the button click

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,21 @@ import './App.css'
 import ButtonComponent from "./components/ButtonComponent.tsx";
 
 function App() {
+
+    let content : string = "DashBoard"
+
     function handleClick(type:string){
         console.log('click')
         alert(type + ' clicked')
+        content = type
     }
     return (
         <>
             <ButtonComponent name = "DashBoard" onClick={handleClick}></ButtonComponent>
             <ButtonComponent name = "Add Customer" onClick={handleClick}></ButtonComponent>
             <ButtonComponent name = "Delete Customer" onClick={handleClick}></ButtonComponent>
+            <br/>
+            {content}
         </>
     )
 }
