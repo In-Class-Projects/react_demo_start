b533fa3bea833b17924b0f21a7c96daf0a4a0c3a
PasinduMshan <madushan01722@gmail.com>
2024-11-27 14:20:19 +0530

    add the string const value

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,12 @@ import {FancyButton, SmallButton} from "./Button.tsx"
 import Button from "./Button.tsx"
 function App() {
 
+    const content : string = "Hello, World!"
+
     return (
         <>
+            {1+1}
+            {content}
             <Button></Button>
             <FancyButton/>
             <SmallButton/>
