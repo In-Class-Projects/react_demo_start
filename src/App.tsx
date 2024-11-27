a10e24227b18589b43581cdfafbb593fe3266d8a
PasinduMshan <madushan01722@gmail.com>
2024-11-27 16:22:56 +0530

    Create Increase the value when the button click part in single line

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,14 @@
 import './App.css'
 
-import AddValueButton from "./components/addValueButton";
 import {useState} from "react";
 
 function App() {
 
-    // let content : string = "DashBoard"
     const [count, setCount] = useState(0);
-
-    function handleClick(type){
-        setCount(count+1)
-    }
 
     return (
         <>
-            <AddValueButton name = {"Count : "+ count} count={count} onClick={handleClick}></AddValueButton>
+            <button onClick={() => setCount(count + 1)}>Click : {count}</button>
         </>
     )
 }
