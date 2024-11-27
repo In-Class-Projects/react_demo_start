dd97f540a25e6a159b7c0de51db71198ba0215bd
PasinduMshan <madushan01722@gmail.com>
2024-11-27 14:29:25 +0530

    use the array to get value and pass

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,47 @@ import Item from "./components/item.tsx";
 import {Customer} from "./components/item.tsx";
 
 function App() {
-
     return (
         <>
-            <Item title={"RAD 1"} desc={"Lorem ipsum dolor sit amet, consectetur adipisicing elit."}></Item>
-            <Item title={"RAD 2"} desc={"Lorem ipsum dolor sit amet, consectetur adipisicing elit."}></Item>
-            <Item title={"RAD 3"} desc={"Lorem ipsum dolor sit amet, consectetur adipisicing elit."}></Item>
+            <Item title={itemsList[0].title} desc={itemsList[0].desc}></Item>
+            <Item title={itemsList[1].title} desc={itemsList[1].desc}></Item>
+            <Item title={itemsList[2].title} desc={itemsList[2].desc}></Item>
 
-            <Customer title={"Customer 1"} desc={"Lorem ipsum dolor sit amet, consectetur adipisicing elit."}></Customer>
-            <Customer title={"Customer 2"} desc={"Lorem ipsum dolor sit amet, consectetur adipisicing elit."}></Customer>
-            <Customer title={"Customer 3"} desc={"Lorem ipsum dolor sit amet, consectetur adipisicing elit."}></Customer>
+            <Customer {...customerList[0]}/>
+            <Customer {...customerList[1]}/>
+            <Customer {...customerList[3]}/>
         </>
     )
 }
 
+const itemsList = [
+    {
+        title: "Item 1",
+        desc: "Lorem ipsum dolor sit amet",
+    },
+    {
+        title: "Item 2",
+        desc: "Lorem ipsum dolor sit amet",
+    },
+    {
+        title: "Item 3",
+        desc: "Lorem ipsum dolor sit amet",
+    }
+]
+
+const customerList = [
+    {
+        title: "Customer Boy",
+        desc: "Lorem ipsum dolor sit amet",
+    },
+    {
+        title: "Customer Girl",
+        desc: "Lorem ipsum dolor sit amet",
+    },
+    {
+        title: "Customer other",
+        desc: "Lorem ipsum dolor sit amet",
+    }
+]
 
 export default App
