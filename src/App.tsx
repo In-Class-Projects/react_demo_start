dcf8b8d438591be4e132a94b96e64203cc79d36c
PasinduMshan <madushan01722@gmail.com>
2024-11-27 14:31:41 +0530

    use the map() & for loop to get array values

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,57 @@
 import './App.css'
+
 import Item from "./components/item.tsx";
 import {Customer} from "./components/item.tsx";
 
 function App() {
+
+    const itemsList = [
+        {
+            title: "Item 1",
+            desc: "Lorem ipsum dolor sit amet",
+        },
+        {
+            title: "Item 2",
+            desc: "Lorem ipsum dolor sit amet",
+        },
+        {
+            title: "Item 3",
+            desc: "Lorem ipsum dolor sit amet",
+        }
+    ]
+
+    const customerList = [
+        {
+            title: "Customer Boy",
+            desc: "Lorem ipsum dolor sit amet",
+        },
+        {
+            title: "Customer Girl",
+            desc: "Lorem ipsum dolor sit amet",
+        },
+        {
+            title: "Customer other",
+            desc: "Lorem ipsum dolor sit amet",
+        }
+    ]
+
+    const customerArray = [];
+
+    for (const cusArrayElement of customerList) {
+        customerArray.push(
+            <Customer title={cusArrayElement.title} desc={cusArrayElement.desc}/>
+        );
+    }
+
     return (
         <>
-            <Item title={itemsList[0].title} desc={itemsList[0].desc}></Item>
-            <Item title={itemsList[1].title} desc={itemsList[1].desc}></Item>
-            <Item title={itemsList[2].title} desc={itemsList[2].desc}></Item>
+            {itemsList.map(item => (
+                <Item title={item.title} desc={item.desc}/>
+            ))}
 
-            <Customer {...customerList[0]}/>
-            <Customer {...customerList[1]}/>
-            <Customer {...customerList[3]}/>
+            {customerArray}
         </>
     )
 }
-
-const itemsList = [
-    {
-        title: "Item 1",
-        desc: "Lorem ipsum dolor sit amet",
-    },
-    {
-        title: "Item 2",
-        desc: "Lorem ipsum dolor sit amet",
-    },
-    {
-        title: "Item 3",
-        desc: "Lorem ipsum dolor sit amet",
-    }
-]
-
-const customerList = [
-    {
-        title: "Customer Boy",
-        desc: "Lorem ipsum dolor sit amet",
-    },
-    {
-        title: "Customer Girl",
-        desc: "Lorem ipsum dolor sit amet",
-    },
-    {
-        title: "Customer other",
-        desc: "Lorem ipsum dolor sit amet",
-    }
-]
 
 export default App
