dd77f4d9a8716ce652ca1723e8f577c39badb110
PasinduMshan <madushan01722@gmail.com>
2024-11-27 14:25:52 +0530

    pass the object type & normal value to function

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,21 @@
 import './App.css'
-import {FancyButton, SmallButton} from "./Button.tsx"
-import Button from "./Button.tsx"
+import Item from "./components/item.tsx";
+import {Customer} from "./components/item.tsx";
+
 function App() {
-
-    const content : string = "Hello, World!"
-
-    const bool : boolean = true;
-    let contentOfBool : string;
-
-    if (bool) {
-        contentOfBool = "True";
-    } else {
-        contentOfBool = "False";
-    }
-
 
     return (
         <>
-            {1+1}
-            {content}
-            {contentOfBool}
-            {bool ? "True" : "False"}
-            <Button></Button>
-            <FancyButton/>
-            <SmallButton/>
+            <Item title={"RAD 1"} desc={"Lorem ipsum dolor sit amet, consectetur adipisicing elit."}></Item>
+            <Item title={"RAD 2"} desc={"Lorem ipsum dolor sit amet, consectetur adipisicing elit."}></Item>
+            <Item title={"RAD 3"} desc={"Lorem ipsum dolor sit amet, consectetur adipisicing elit."}></Item>
+
+            <Customer title={"Customer 1"} desc={"Lorem ipsum dolor sit amet, consectetur adipisicing elit."}></Customer>
+            <Customer title={"Customer 2"} desc={"Lorem ipsum dolor sit amet, consectetur adipisicing elit."}></Customer>
+            <Customer title={"Customer 3"} desc={"Lorem ipsum dolor sit amet, consectetur adipisicing elit."}></Customer>
         </>
     )
 }
+
 
 export default App
