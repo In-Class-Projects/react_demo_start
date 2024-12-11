82cd4447c8175aa5e108470b330b15aa82c8b5f8
PasinduMshan <madushan01722@gmail.com>
2024-12-11 09:40:35 +0530

    save data with using previous state and use function

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,16 @@ function App() {
         lastName: "",
     });
 
+    function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
+        setCustomer({
+            ...customer,
+            [e.target.name]: e.target.value,
+        })
+    }
+
     return (
         <>
-            <input type="text" placeholder={"First Name"} onChange= {(e) =>{
-                setCustomer({...customer, firstName : e.target.value});
-            }}/>
+            <input name={"firstName"} type="text" placeholder={"First Name"} onChange= {(e) =>{handleInputChange(e)}}/>
             <input type="text" placeholder={"Last Name"} onChange= {(e) =>{
                 setCustomer({...customer, lastName : e.target.value});
             }}/>
