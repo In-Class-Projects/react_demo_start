55f84a1a044a15edaee6d175077f6c85def92520
PasinduMshan <madushan01722@gmail.com>
2024-12-11 10:53:05 +0530

    add customer data and use validate customer data use key attribute

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,28 @@
 import './App.css'
 import {useState} from "react";
+import {Customer} from "./models/Customer.ts";
 
 
 function App() {
+    const [customers, setCustomers] = useState<Customer[]>([])
 
-    const [customer, setCustomer] = useState({
-        firstName: "",
-        lastName: "",
-    });
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
+    const [address, setAddress] = useState("");
 
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
+    const addCustomer = () => {
+        const newCustomer = new Customer(name, email, phone, address);
 
-    function addCustomer() {
-        setCustomer({
-            ...customer,
-            firstName: firstName,
-            lastName: lastName,
-        })
+        setCustomers((customers) => [...customers, newCustomer]);
     }
 
     return (
         <>
-            <input name={"firstName"} type="text" placeholder={"First Name"} onChange= {(e) =>{
-                setFirstName(e.target.value)
-            }}/>
-            <input name={"lastName"} type="text" placeholder={"Last Name"} onChange= {(e) =>{
-                setLastName(e.target.value)
-            }}/>
+            <input name={"name"} type="text" placeholder={"Name"} onChange={(e) => setName(e.target.value)}/>
+            <input name={"email"} type="text" placeholder={"Email"} onChange={(e) => setEmail(e.target.value)}/>
+            <input name={"phone"} type="text" placeholder={"Phone Number"} onChange={(e) => setPhone(e.target.value)}/>
+            <input name={"address"} type="text" placeholder={"Address"} onChange={(e) => setAddress(e.target.value)}/>
 
             <br/>
 
@@ -35,7 +30,11 @@ function App() {
 
             <br/>
 
-            {customer.firstName + " " + customer.lastName}
+            {customers.map(customer => (
+                <h2 key={customer.email}>{customer.name + " " + customer.email + " " + customer.phone + " " + customer.address}</h2>
+            ))}
+
+
         </>
     )
 }
