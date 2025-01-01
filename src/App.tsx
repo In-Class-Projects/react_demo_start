f0bfb25def3b2ba761be30dee173222e33f55c80
PasinduMshan <madushan01722@gmail.com>
2025-01-01 21:56:42 +0530

    create counter add and remove with useReducer hook and create it with CountReducer.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,6 @@
 import './App.css'
 import {useReducer} from "react";
-
-const initialState = 0;
-
-function CountReducer ( state:number = initialState , action: {type: string, payload: number} ) {
-    switch(action.type) {
-        case 'ADD_COUNTER' :
-            return state + action.payload;
-        case 'REMOVE_COUNTER' :
-            return state - action.payload;
-        default:
-            return state;
-    }
-}
+import {CountReducer, initialState} from "./reducers/CountReducer.ts";
 
 function App() {
 
