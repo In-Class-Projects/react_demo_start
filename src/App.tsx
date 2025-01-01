a1e209abe09e5ab1aa7a069e4635331286140938
PasinduMshan <madushan01722@gmail.com>
2025-01-01 22:03:48 +0530

    print fullname using reducer hook sir code change

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,30 @@
+
+first App.tsx
+
 import './App.css'
 import {useReducer, useState} from "react";
 import {initialState, NameReducer} from "./reducers/NameReducer.ts";
+import {FullName} from "./models/FullName.ts";
 
 function App() {
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
 
     const [names, dispatch] = useReducer(NameReducer, initialState)
+
+    function onSubmit() {
+        const newFullName = new FullName(firstName, lastName);
+        dispatch({type : 'PRINT', payload : newFullName})
+    }
     return (
         <>
             <input  type="text" placeholder={"First Name"}  onChange={(e) => setFirstName(e.target.value)} />
             <input type="text" placeholder={"Last Name"} onChange={(e)=>setLastName(e.target.value)}/>
-            <button onClick={()=> dispatch({type: 'PRINT', payload: {firstName, lastName}})}>Print</button>
+            <button onClick={()=> onSubmit()}>Print</button>
 
             <br/>
 
-            {names.firstName + ' ' + names.lastName}
-
+            {names.map((names : FullName ) => (<div>{names.firstName + ' ' + names.lastName}</div>))}
         </>
     )
 }
--- src/reducers/NameReducer.ts
+++ src/reducers/NameReducer.ts
@@ -6,11 +6,7 @@ export const initialState = {
 export function NameReducer ( state = initialState , action: {type: string, payload: { firstName: string; lastName: string } } ) {
     switch(action.type) {
         case 'PRINT' :
-            return {
-                ...state,
-                firstName : action.payload.firstName,
-                lastName : action.payload.lastName,
-            };
+            return state = action.payload;
         default:
             return state;
     }
